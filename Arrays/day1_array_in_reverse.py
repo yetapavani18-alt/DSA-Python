76a86ee8f27f8751ac0260d82bac163134517cde
yetapavani18-alt <yetapavani18@gmail.com>
2026-07-10 15:04:03 +0530

    add counting zeros

---
+++ Arrays/day1_array_in_reverse.py
@@ -0,0 +1,3 @@
+arr=[1,2,3,4,5,7]
+for i in range(len(arr)-1,-1,-1):
+  print(arr[i],end=" ")
---
+++ Arrays/day1_zero_count.py
@@ -0,0 +1,6 @@
+arr=[0,1,0,3,0,5]
+count=0
+for i in range(len(arr)):
+  if arr[i]==0:
+    count=count+1
+print("count:",count)
